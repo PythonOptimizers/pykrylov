3d209405860abbe175683c0c696f2c3375c448f0
Dominique Orban <dominique.orban@gmail.com>
2012-02-03 21:20:09 -0500

    Import CRAIG.

--- pykrylov/lls/__init__.py
+++ pykrylov/lls/__init__.py
@@ -1,6 +1,7 @@
 """Iterative Methods for Linear Least-Squares Problems"""
 
-from lsqr import *
-from lsmr import *
+from lsqr  import *
+from lsmr  import *
+from craig import *
 
 __all__ = filter(lambda s:not s.startswith('_'), dir())
