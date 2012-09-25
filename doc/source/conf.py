bccea1023fe8a638395e6dacdca0ccb715478fab
Dominique Orban <dominique.orban@gmail.com>
2012-09-25 17:01:55 -0400

    Add package amsfonts for LaTeX doc.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -199,7 +199,7 @@ latex_documents = [
 #latex_use_parts = False
 
 # Additional stuff for the LaTeX preamble.
-#latex_preamble = ''
+latex_preamble = '\usepackage{amsfonts}'
 
 # Documents to append as an appendix to all manuals.
 #latex_appendices = []
