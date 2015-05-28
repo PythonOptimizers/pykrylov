ccb3932f1f36f6ae7610cae8639d3f103610abc1
Dominique Orban <dominique.orban@gmail.com>
2015-05-28 18:06:26 -0400

    Install linop tests.

--- pykrylov/linop/setup.py
+++ pykrylov/linop/setup.py
@@ -6,6 +6,8 @@ def configuration(parent_package='', top_path=None):
 
     config = Configuration('linop', parent_package, top_path)
 
+    config.add_data_dir('tests')
+
     return config
 
 if __name__ == '__main__':
